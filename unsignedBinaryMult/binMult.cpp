cb75e5be8307cf2d20466ed97422249f9669f9c3
MattDN93 <matthew.deneef@gmail.com>
2015-05-21 19:39:52 +0200

    *Added 16-bit support *prints steps of mult to file

--- unsignedBinaryMult/binMult.cpp
+++ unsignedBinaryMult/binMult.cpp
@@ -8,6 +8,7 @@
 using namespace std;
 
 #define DEBUG 1
+#define BITSIZE 16
 
 class unsignedBinMult
 {
@@ -18,6 +19,8 @@ public:
 	void toBinary(int,int);
 	void sum();
 	void rightShift();
+	void doMult();				//perform the multiplication
+	void outIntArr(vector <int>);		//displays integer content to user
 
 	ifstream inValues;
 	ofstream outValues;
@@ -26,14 +29,18 @@ public:
 	int count;				//count variable
 
 	int input_M = 0;			//multiplicand in integer form
-	string m_string;
+	string m_string="";		//stored string rep of binary value
 	vector <int> bin_M;		//multiplicand in binary form
 
 	int input_Q = 0;			//multiplier in integer form
-	string q_string;
+	string q_string="";		//stored string rep of binary value
 	vector <int> bin_Q;		//multiplier in binary form
 
 	vector <int> acc;		//accumulator
+	string acc_string = "";
+
+	string adding = "C,A <- A + M";
+	string r_shifting = "RightShift";
 
 };
 
@@ -51,32 +58,45 @@ void main(int argc, char** argv)
 
 	if (DEBUG)
 	{
-		ubm.input_M = 12/*atoi(argv[1])*/;
-		ubm.input_Q = 13/*atoi(argv[2])*/;
+		ubm.input_M = 12;
+		ubm.input_Q = 13;
 	}
 	if (!DEBUG)
 	{
-		ubm.input_M = atoi(argv[1]);
-		ubm.input_Q = atoi(argv[2]);
+		ubm.input_M = atoi(argv[1]);				//receive args
+		ubm.input_Q = atoi(argv[2]);				//receive args from VB
 	}
 
 
 	ubm.outValues.open("valuesToDisplay.txt");
 	//ubm.outValues << "Successfully read from C++ execution." << endl;
 	ubm.outValues << "\nValues Entered:" << endl;
-	ubm.outValues << ubm.input_M <<"\t"<< ubm.input_Q;
+	ubm.outValues << ubm.input_M <<"\t"<< ubm.input_Q;	//read from VB process
 
 	ubm.toBinary(ubm.input_M, ubm.input_Q);			//convert M and Q to binary
-
+	ubm.doMult();									// do the multiplication
 
 
 }
 
+void unsignedBinMult::outIntArr(vector <int> arr)
+{
+	for (int i = 0; i < BITSIZE; i++)
+	{
+		outValues << arr[i];
+	}
+}
+
 void unsignedBinMult::toBinary(int m_in, int q_in)
 {
-	bin_M.resize(8);
-	bin_Q.resize(8);
-	int b = 8;
+	bin_M.resize(BITSIZE);
+	bin_Q.resize(BITSIZE);
+	m_string.resize(BITSIZE);
+	q_string.resize(BITSIZE);
+
+
+
+	int b = BITSIZE;
 
 	outValues << "\nBinary Value Built:" << endl;
 	outValues << "\nMultiplicand(M):";
@@ -89,15 +109,15 @@ void unsignedBinMult::toBinary(int m_in, int q_in)
 		m_in /= 2;
 	}
 
-	for (int i = 0; i < 8; i++)
+	for (int i = 0; i < BITSIZE; i++)
 	{
-		outValues << bin_M[i];
+		outValues << bin_M[i];			//write to file
 	}
 
 	outValues << "\n\nMultiplier (Q):" << endl;
 	outValues << "--------------------------" << endl;
 
-	b = 8;							//reset the value of the counter 
+	b = BITSIZE;							//reset the value of the counter 
 
 	while (q_in != 0)
 	{
@@ -106,7 +126,7 @@ void unsignedBinMult::toBinary(int m_in, int q_in)
 		q_in /= 2;
 	}
 
-	for (int i = 0; i < 8; i++)
+	for (int i = 0; i < BITSIZE; i++)
 	{
 		outValues << bin_Q[i];
 	}
@@ -118,4 +138,18 @@ void unsignedBinMult::toBinary(int m_in, int q_in)
 	}
 
 
+}
+
+void unsignedBinMult::doMult()
+{
+	acc.resize(BITSIZE);
+	count = 8;
+
+	outValues << "\n\n-----------------------" << endl;
+	outValues << "\nStep\tProcess\t\t Multiplicand (M) \t Accumulator (A) \t Multiplier(Q) \tCount(n)" << endl;
+	outValues << "\n" << count << "\t\tInit\t\t"; outIntArr(bin_M); outValues << "\t"; outIntArr(acc); outValues << "\t"; outIntArr(bin_Q); outValues << "\t" << count << endl;
+	if (bin_Q[0] == 1)
+	{
+
+	}
 }
